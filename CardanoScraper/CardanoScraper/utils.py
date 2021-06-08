e90731d11839f8ec3b43031666f43bcf641fe895
van010@github.com <itmannavys@gmail.com>
2021-06-08 14:28:07 +0700

    cardano news: add text colors to easy debug & fix error 429

--- CardanoScraper/CardanoScraper/utils.py
+++ CardanoScraper/CardanoScraper/utils.py
@@ -21,3 +21,18 @@ def create_data_directory():
 
 
 create_data_directory()
+
+
+def colors_mark():
+	colors = {
+		'header': '\033[95m',
+		'okblue': '\033[94m',
+		'okcyan': '\033[96m',
+		'okgreen': '\033[92m',
+		'warning': '\033[93m',
+		'fail': '\033[91m',
+		'endc': '\033[0m',
+		'bold': '\033[1m',
+		'underline': '\033[4m',
+	}
+	return colors
