28de30074f024944bb68e9715a9b4a1697b8c9fd
van010@github.com <itmannavys@gmail.com>
2021-06-14 13:59:47 +0700

    update crawl cardar news data

--- CardanoScraper/CardanoScraper/spiders/carda_news_spider.py
+++ CardanoScraper/CardanoScraper/spiders/carda_news_spider.py
@@ -4,6 +4,7 @@ import time
 import json
 from .. import utils
 from scrapy import Request
+from scrapy.http import HtmlResponse, Request
 
 color = utils.colors_mark()
 mongoClient = pymongo.MongoClient("mongodb://root:password@localhost:27017/")
@@ -109,57 +110,6 @@ class CardanoNewsContent(scrapy.Spider):
 		yield data
 		time.sleep(4)
 
-
-# class CardaNewsContent(scrapy.Spider):
-# 	name = "crawlAllCarda"
-# 	start_urls = [
-# 		'https://forum.cardano.org/c/english/announcements/13',
-# 	]
-#
-# 	def __init__(self, **kwargs):
-# 		super().__init__(**kwargs)
-# 		self.mongoClient = pymongo.MongoClient("mongodb://root:password@localhost:27017/")
-# 		self.myDatabase = self.mongoClient['cardanoNews']
-#
-# 	def parse(self, response, **kwargs):
-# 		post_links = response.css('.link-top-line a')
-# 		# print(f'post_links {color["header"]} {post_links} {color["endc"]}')
-# 		yield from response.follow_all(post_links, self.parse_content)
-#
-# 		pagination_links = response.css('span b a')
-# 		print(f'pagination_links {color["header"]} {response.url} {color["endc"]}')
-# 		yield from response.follow_all(pagination_links, self.parse)
-# 		posts = response.css('tr.topic-list-item')
-# 		for post in posts:
-# 			data = {
-# 				'title': post.css('td.main-link span.link-top-line a.raw-topic-link::text').get(),
-# 				'tags': post.css('td.main-link div.link-bottom-line div.discourse-tags a::text').getall(),
-# 				'link_tags': post.css('td.main-link div.link-bottom-line div.discourse-tags a::attr(href)').getall(),
-# 				'link_post': post.css('td.main-link span.link-top-line a::attr(href)').get(),
-# 				'posters': post.css('td.posters a::attr(href)').getall(),
-# 				'avatars': post.css('td.posters a img::attr(src)').getall(),
-# 				'replies': post.css('td.replies span.posts::text').get(),
-# 				'views': post.css('td.views span.views::text').get(),
-# 				'date': post.css('td::text').getall()[-1].strip(),
-# 				'latest': 0,
-# 				'approve': 1
-# 			}
-# 			yield data
-#
-# 	def parse_content(self, response, **kwargs):
-# 		def extraction_with_css(query):
-# 			return response.css(query).get(default='').strip()
-#
-# 		data = {
-# 			'raw_content': extraction_with_css('div.post'),
-# 			'link_content': extraction_with_css('div.crawler-post-meta span + link::attr(href)'),
-# 			'post_time': extraction_with_css('time.post-time::text'),
-# 			'latest': 0,
-# 		}
-# 		yield data
-# 		time.sleep(5)
-
-
 class IohkContent(scrapy.Spider):
 	name = "iohk"
 
@@ -175,9 +125,9 @@ class IohkContent(scrapy.Spider):
 			"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36",
 		}
 		for i in range(total_page):
-			yield Request(url=url.format(i), callback=self.parse, headers=headers)
+			yield Request(url=url.format(i), callback=self.parse_item, headers=headers)
 
-	def parse(self, response):
+	def parse_item(self, response):
 		# page = response.url
 		# utils.save_to_html("all", response.body)
 		content = json.loads(response.body)
